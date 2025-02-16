901a62cd2e5c6342ea3bc41488f81f15a0bf889c
Caio Maia <caiomaia3@gmail.com>
2025-02-16 02:34:13 -0300

    change terraform secret

--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -6,6 +6,6 @@ resource "kubernetes_secret" "this" {
 
   data = {
     MSSQL_SA_PASSWORD = var.mssql_sa_password
-    SVC_PASS          = base64encode(var.svc_pass)
+    SVC_PASS          = var.svc_pass
   }
 }
