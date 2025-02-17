8dd374a2a30439f9e1edcd6745c7667bbb1fbee2
Guilherme Pereira <guilherme.cbpereira@gmail.com>
2025-02-17 22:28:42 +0000

    Merged PR 23: Revert 'Update branch'
    Reverts !22

--- terraform/modules/aks/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "kubelet_identity_object_id" {
-  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-}
--- terraform/modules/secrets/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "secret_name" {
-  description = "O nome do Secret criado"
-  value       = kubernetes_secret.this.metadata[0].name
-}
